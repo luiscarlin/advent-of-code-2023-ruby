663f801bd9c455b5d07b05d22fb6aafce15d9bdc
Luis Carlin <luiscarlin@gmail.com>
2023-12-01 22:53:09 -0500

    add rubocop and gemfile

--- day01/day01.rb
+++ day01/day01.rb
@@ -1,32 +1,30 @@
-def part1
-    lines = File.readlines(File.join(__dir__, 'input.txt'), chomp: true).map { |line|
-        numbers = line.scan(/\d/)
-        (numbers.first + numbers.last).to_i
-    }
+# frozen_string_literal: true
 
-    lines.sum
+def part1
+  lines = File.readlines(File.join(__dir__, 'input.txt'), chomp: true).map do |line|
+    numbers = line.scan(/\d/)
+
+    (numbers.first + numbers.last).to_i
+  end
+  lines.sum
 end
 
 def part2
-    lines = File.readlines(File.join(__dir__,'input.txt'), chomp: true).map { |line|
-        matches = []
+  lines = File.readlines(File.join(__dir__, 'input.txt'), chomp: true).map do |line|
+    matches = []
 
-        for i in 0..line.length - 1
-            if line[i].to_i.to_s == line[i]
-                matches << line[i]
-            end
+    (0..line.length - 1).each do |i|
+      matches << line[i] if line[i].to_i.to_s == line[i]
 
-            %w[one two three four five six seven eight nine].each_with_index do |val, d|
-                if line[i..].start_with?(val)
-                    matches << (d+1).to_s
-                end
-            end
-        end
+      %w[one two three four five six seven eight nine].each_with_index do |val, d|
+        matches << (d + 1).to_s if line[i..].start_with?(val)
+      end
+    end
 
-        (matches.first + matches.last).to_i
-    }
-    lines.sum
+    (matches.first + matches.last).to_i
+  end
+  lines.sum
 end
 
-puts "part 1: #{part1()}"
-puts "part 2: #{part2()}" 
+puts "part 1: #{part1}"
+puts "part 2: #{part2}"
