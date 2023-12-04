506afcd920f6135bf02715ed9b8c92c67938d8fb
Luis Carlin <luiscarlin@gmail.com>
2023-12-04 05:53:34 -0500

    day4 part 2 done

--- day04/day04.rb
+++ day04/day04.rb
@@ -30,8 +30,35 @@ def part1
 end
 
 def part2
-  File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+  winning = []
+  yours = []
+  num_matches = []
+
+  File.readlines(File.join(__dir__, 'input.txt'), chomp: true).each do |line|
+    _, line_winning, line_yours = line.split(/[:|]/)
+
+    winning << line_winning.split(' ').map(&:to_i)
+    yours << line_yours.split(' ').map(&:to_i)
+  end
+
+  num_of_cards = Array.new(winning.length, 1)
+
+  winning.length.times do |i|
+    num_matches[i] = yours[i].intersection(winning[i]).length
+  end
+
+  num_matches.each_with_index do |matches, currect_card_index|
+    next if matches.zero?
+
+    num_of_cards[currect_card_index].times do
+      (1..matches).each do |i|
+        num_of_cards[i + currect_card_index] += 1
+      end
+    end
+  end
+
+  num_of_cards.sum
 end
 
 puts "part 1: #{part1}"
-# puts "part 2: #{part2}"
+puts "part 2: #{part2}"
