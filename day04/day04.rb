74f70146389d1e442caaf49d017e6ea95022cf98
Luis Carlin <luiscarlin@gmail.com>
2023-12-04 06:26:51 -0500

    refactor day 4

--- day04/day04.rb
+++ day04/day04.rb
@@ -5,23 +5,19 @@ def part1
   yours = []
   points = 0
 
-  File.readlines(File.join(__dir__, 'input.txt'), chomp: true).each do |line|
+  File.foreach(File.join(__dir__, 'input.txt')) do |line|
     _, line_winning, line_yours = line.split(/[:|]/)
 
-    winning << line_winning.split(' ').map(&:to_i)
-    yours << line_yours.split(' ').map(&:to_i)
+    winning << line_winning.split.map(&:to_i)
+    yours << line_yours.split.map(&:to_i)
   end
 
-  winning.length.times do |i|
-    num_matches = yours[i].intersection(winning[i]).length
+  winning.each_with_index do |winning_numbers, i|
+    num_matches = (yours[i] & winning_numbers).length
 
     next if num_matches.zero?
 
-    card_point = 1
-
-    (1...num_matches).each do
-      card_point *= 2
-    end
+    card_point = 2**(num_matches - 1)
 
     points += card_point
   end
@@ -32,28 +28,24 @@ end
 def part2
   winning = []
   yours = []
-  num_matches = []
+  num_of_cards = []
 
-  File.readlines(File.join(__dir__, 'input.txt'), chomp: true).each do |line|
+  File.foreach(File.join(__dir__, 'input.txt')) do |line|
     _, line_winning, line_yours = line.split(/[:|]/)
 
-    winning << line_winning.split(' ').map(&:to_i)
-    yours << line_yours.split(' ').map(&:to_i)
+    winning << line_winning.split.map(&:to_i)
+    yours << line_yours.split.map(&:to_i)
   end
+
+  num_matches = yours.zip(winning).map { |y, w| (y & w).length }
 
   num_of_cards = Array.new(winning.length, 1)
 
-  winning.length.times do |i|
-    num_matches[i] = yours[i].intersection(winning[i]).length
-  end
-
-  num_matches.each_with_index do |matches, currect_card_index|
+  num_matches.each_with_index do |matches, current_card_index|
     next if matches.zero?
 
-    num_of_cards[currect_card_index].times do
-      (1..matches).each do |i|
-        num_of_cards[i + currect_card_index] += 1
-      end
+    num_of_cards[current_card_index..(current_card_index + matches)].each do |card|
+      card += 1
     end
   end
 
