f4215a217776a8bfe923525eacc85a21ce8c0e79
Luis Carlin <luiscarlin@gmail.com>
2023-12-07 17:37:35 -0500

    refactor day6

--- day06/day06.rb
+++ day06/day06.rb
@@ -3,25 +3,12 @@
 def part1
   lines = File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
 
-  times = lines[0].scan(/\d+/).map(&:to_i)
-  distance = lines[1].scan(/\d+/).map(&:to_i)
+  times, distances = lines.map { |line| line.scan(/\d+/).map(&:to_i) }
 
-  total_ways_to_reach = []
+  total_ways_to_reach = times.map.with_index do |time, i|
+    distance_to_cover = distances[i]
 
-  times.each_with_index do |time, i|
-    distance_to_cover = distance[i]
-
-    num_ways_to_reach_distance = 0
-
-    (0..time).each do |hold|
-      time_running = time - hold
-
-      distance_covered = time_running * hold
-
-      num_ways_to_reach_distance += 1 if distance_covered > distance_to_cover
-    end
-
-    total_ways_to_reach << num_ways_to_reach_distance
+    (0..time).count { |hold| (time - hold) * hold > distance_to_cover }
   end
 
   total_ways_to_reach.reduce(:*)
@@ -30,22 +17,13 @@ end
 def part2
   lines = File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
 
-  time = lines[0].scan(/\d+/).join.to_i
-  distance = lines[1].scan(/\d+/).join.to_i
+  time, distance = lines.map { |line| line.scan(/\d+/).join.to_i }
 
-  distance_to_cover = distance
-
-  num_ways_to_reach_distance = 0
-
-  (0..time).each do |hold|
+  (0..time).count do |hold|
     time_running = time - hold
-
     distance_covered = time_running * hold
-
-    num_ways_to_reach_distance += 1 if distance_covered > distance_to_cover
+    distance_covered > distance
   end
-
-  num_ways_to_reach_distance
 end
 
 puts "part 1: #{part1}"
