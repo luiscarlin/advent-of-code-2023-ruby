2b3a25b7c4c3869ba1e47f35572ac2817cbd0fb8
Luis Carlin <luiscarlin@gmail.com>
2023-12-24 12:22:59 -0500

    day 8 part 1 done

--- day08/day08.rb
+++ day08/day08.rb
@@ -1,11 +1,42 @@
 # frozen_string_literal: true
 
 def part1
-  File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+  lines = File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+
+  instructions = lines.shift
+
+  graph = {}
+
+  lines.each do |line|
+    next if line.empty?
+
+    node, left, right = line.scan(/\w+/)
+
+    graph[node] = { right:, left: }
+  end
+
+  node = 'AAA'
+  steps = 0
+
+  while node != 'ZZZ'
+    direction = instructions[steps % instructions.length]
+
+    if direction == 'R'
+      node = graph[node][:right]
+    elsif direction == 'L'
+      node = graph[node][:left]
+    end
+
+    steps += 1
+  end
+
+  steps
 end
 
 def part2
   File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+
+  'not yet implemented'
 end
 
 puts "part 1: #{part1}"
