7824c228ffe20643192d4d1efa669fc20629d89c
Luis Carlin <luiscarlin@gmail.com>
2023-12-26 10:43:41 -0500

    day 9 part 1 done

--- day09/day09.rb
+++ day09/day09.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
 def part1
-  File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+  lines = File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
 
-  'not yet implemented'
+  predicted_values = []
+
+  lines.each do |line| 
+    history_line = line.split(' ').map(&:to_i)
+
+    diffs = [history_line]
+
+    while !diffs.last.all? { |num| num == 0 } do
+      line_to_process = diffs.last
+
+      new_row = line_to_process.each_cons(2).map { |a, b| b - a }
+
+      diffs << new_row
+    end
+
+    predicted_values << diffs.map(&:last).sum
+  end
+
+  predicted_values.sum
 end
 
 def part2
@@ -13,4 +31,4 @@ def part2
 end
 
 puts "part 1: #{part1}"
-puts "part 2: #{part2}"
+# puts "part 2: #{part2}"
