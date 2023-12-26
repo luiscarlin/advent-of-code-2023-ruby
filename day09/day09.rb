4207e83d175700e36edc5775a96533d2f98cb4be
Luis Carlin <luiscarlin@gmail.com>
2023-12-26 11:43:24 -0500

    day 9 part 2 done

--- day09/day09.rb
+++ day09/day09.rb
@@ -5,12 +5,12 @@ def part1
 
   predicted_values = []
 
-  lines.each do |line| 
+  lines.each do |line|
     history_line = line.split(' ').map(&:to_i)
 
     diffs = [history_line]
 
-    while !diffs.last.all? { |num| num == 0 } do
+    until diffs.last.all?(&:zero?)
       line_to_process = diffs.last
 
       new_row = line_to_process.each_cons(2).map { |a, b| b - a }
@@ -25,10 +25,37 @@ def part1
 end
 
 def part2
-  File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+  lines = File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
 
-  'not yet implemented'
+  predicted_values = []
+
+  lines.each do |line|
+    history_line = line.split(' ').map(&:to_i)
+
+    diffs = [history_line]
+
+    until diffs.last.all?(&:zero?)
+      line_to_process = diffs.last
+
+      new_row = line_to_process.each_cons(2).map { |a, b| b - a }
+
+      diffs << new_row
+    end
+
+    rolling = 0
+
+    first_nums = diffs.map(&:first).reverse
+
+    first_nums.each_with_index do |val, index|
+      next if index.zero?
+
+      rolling = val - rolling
+    end
+    predicted_values << rolling
+  end
+
+  predicted_values.sum
 end
 
 puts "part 1: #{part1}"
-# puts "part 2: #{part2}"
+puts "part 2: #{part2}"
