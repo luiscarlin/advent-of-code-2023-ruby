9d88b5abfa7751bbb9c4148d2280d897d4509dcd
Luis Carlin <luiscarlin@gmail.com>
2023-12-30 12:29:32 -0500

    day 10 part 2 done

--- day10/day10.2.rb
+++ day10/day10.2.rb
@@ -1,89 +1,35 @@
 # frozen_string_literal: true
 
 def part2
-  grid = File.readlines(File.join(__dir__, 'input.txt'), chomp: true).map(&:chars)
+  grid = File.readlines(File.join(__dir__, 'main_pipe.txt'), chomp: true).map(&:chars)
 
-  s_row = grid.find_index { |row| row.include?('S') }
-  s_col = grid[s_row].index('S')
+  answer = 0
 
-  queue = [[s_row, s_col]]
-  visited = []
+  grid.each_with_index do |row, _row_index|
+    row.each_with_index do |char, col_index|
+      next if char != '.'
 
-  while queue.length.positive?
-    current_tile = queue.shift
-
-    neighbors = get_neighbor_tiles(grid, current_tile)
-
-    non_visited_neighbors = neighbors.reject { |neighbor| visited.include?(neighbor) }
-
-    queue += non_visited_neighbors
-    queue.uniq!
-
-    visited << current_tile
-    visited.uniq!
+      num_crossings = get_number_of_crossings(row, col_index)
+      answer += 1 if num_crossings.odd?
+    end
   end
 
-  # print_visible_loop(grid, visited)
-
-  visited.length / 2
+  answer
 end
 
-def get_neighbor_tiles(grid, current_tile)
-  rows = grid.length
-  cols = grid[0].length
+def get_number_of_crossings(row, col_index)
+  count = 0
 
-  current_tile_row = current_tile[0]
-  current_tile_col = current_tile[1]
-  current_tile_value = grid[current_tile_row][current_tile_col]
-
-  directions = [
-    [-1, 0], # up
-    [0, 1], # right
-    [1, 0], # down
-    [0, -1] # left
-  ]
-
-  allowed_from = [
-    ['|', 'J', 'L'], # up
-    ['-', 'L', 'F'], # right
-    ['|', 'F', '7'], # down
-    ['-', 'J', '7'] # left
-  ]
-
-  allowed_to = [
-    ['|', '7', 'F'], # up
-    ['-', 'J', '7'], # right
-    ['|', 'J', 'L'], # down
-    ['-', 'L', 'F'] # left
-  ]
-
-  neighbors = []
-
-  directions.each_with_index do |direction, index|
-    neighbor_tile_row = current_tile[0] + direction[0]
-    neighbor_tile_col = current_tile[1] + direction[1]
-
-    next unless neighbor_tile_row.between?(0, rows - 1) && neighbor_tile_col.between?(0, cols - 1)
-
-    neighbor_tile_value = grid[neighbor_tile_row][neighbor_tile_col]
-
-    next unless allowed_from[index].include?(current_tile_value) || current_tile_value == 'S'
-    next unless allowed_to[index].include?(neighbor_tile_value) || neighbor_tile_value == 'S'
-
-    neighbors << [neighbor_tile_row, neighbor_tile_col]
+  (0...col_index).each do |k|
+    count += 1 if ['J', 'L', '|'].include?(row[k])
   end
-
-  neighbors
+  count
 end
 
-def print_visible_loop(grid, visited)
+def print_grid(grid)
   grid.each_with_index do |row, row_index|
     row.each_with_index do |_col, col_index|
-      if visited.include?([row_index, col_index])
-        print grid[row_index][col_index]
-      else
-        print '.'
-      end
+      print grid[row_index][col_index]
     end
     puts
   end
