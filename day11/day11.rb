2a1eaeaaf9c1bb5609a75d8bab83b803bc12ecbb
Luis Carlin <luiscarlin@gmail.com>
2023-12-30 12:30:11 -0500

    day11 start

---
+++ day11/day11.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def part1
+  File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+
+  'not yet implemented'
+end
+
+def part2
+  File.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+
+  'not yet implemented'
+end
+
+puts "part 1: #{part1}"
+puts "part 2: #{part2}"
